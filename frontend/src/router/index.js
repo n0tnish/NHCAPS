fee65608f61b036c40ecfc1f10d5a30bbeecc271
n0tnish <nishaatgafieldien@gmail.com>
2024-04-06 09:24:49 +0200

    added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,7 +14,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  path: '/signup',
+  name:'signup',
+  component: () => import(/* webpackChunkName: "signup" */ '../views/SignupView.vue')
 ]
 
 const router = createRouter({
